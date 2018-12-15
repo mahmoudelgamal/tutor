e397b02859970faaf23988193c307e0df2466f28
Mahmoud Elgamal <elgamal.mem@gmail.com>
2018-12-16 00:21:55 +0200

    banner fixed

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	//banner height
-	var wh = $(window).height();
-	var nav = $(".navbar").innerHeight();
-	var w_width = $(window).width()
-    var banner = $(".banner").height();
-	$(".banner").innerHeight(wh);
-	$(".overlay").innerHeight(wh);
-})
